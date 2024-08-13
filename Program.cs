c46e550a4ea0bb9b86dcd31ec0056cb5ed7a4b55
Yazmin <moyanoyazminalejandra@gmail.com>
2024-08-13 15:20:13 -0300

    Fin calculadora con operaciones basicas

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+/*
 Console.WriteLine("Hello, World!");
 
 int a ; 
@@ -50,3 +52,76 @@ if (intNum1 > 0 )
 }
 
 Console.WriteLine("El numero invertido es: " + invertido);
+
+/*
+Ingrese al branch CalculadoraV1 y construya un programa que sea una
+calculadora que permita al usuario realizar las 4 operaciones básicas (Sumar, Restar,
+Multiplicar y Dividir) a partir de un menú para seleccionar la opción a elegir y que luego
+pida dos números y que devuelva el resultado de la operación seleccionada. Además
+una vez que termine de realizar la operación le pregunte si desea realizar otro cálculo.
+
+
+*/
+    do
+    {   
+       
+        Console.WriteLine("\nSELECCIONE LA OPERACION : \n 1:SUMAR \n 2:RESTAR \n 3:MULTIPLICAR \n 4:DIVIDIR");
+
+        do
+        {          
+            Console.WriteLine("\nIngresar opcion:");
+            entrada = Console.ReadLine();                
+        } while (!int.TryParse(entrada, out operacion) || (operacion != 1 && operacion!= 2 && operacion != 3 && operacion != 4 && operacion != 5) );
+        
+
+            do
+            {
+                Console.WriteLine("\nIngrese el primer numero:");
+                N1 = Console.ReadLine(); 
+            } while (!float.TryParse(N1, out Num1));
+
+            do
+            {
+                Console.WriteLine("\nIngrese el segundo numero:");
+                N2 = Console.ReadLine(); 
+            } while (!float.TryParse(N2, out Num2));
+
+           switch (operacion)
+           {
+                case 1: // CONTROL DE SUMA
+                    Console.WriteLine("\nEl resultado de la suma es: " + (Num1 + Num2));
+                    break;
+                
+                case 2: // CONTROL DE RESTA
+                    Console.WriteLine("\nEl resultado de la resta es: " + (Num1 - Num2));
+                    break;
+
+                case 3: // CONTROL DE MULTIPLICACIÓN
+                    Console.WriteLine("\nEl resultado de la multiplicación es: " + (Num1 * Num2));
+                    break;
+
+                case 4: // CONTROL DE DIVISIÓN
+                    if (Num2 != 0) 
+                    {
+                        Console.WriteLine("\nEl resultado de la división es: " + (Num1 / Num2));
+                    } 
+                    else 
+                    {
+                        Console.WriteLine("\nNo se puede dividir entre 0");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("\nOperación no válida");
+                    break;
+            }
+                salir = true;
+
+        } else {
+            salir = false;
+        }
+
+    } while (salir);
+
+
+ 
